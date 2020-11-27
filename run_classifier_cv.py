f15332684cf6d1c32e4aa497a61af656c44a33e4
root <root@9f29bba7-d6cd-4bae-882a-b597311021ea.apple.com>
2020-11-27 03:06:57 +0000

    made some modification for test

--- run_classifier_cv.py
+++ run_classifier_cv.py
@@ -7,6 +7,7 @@ import argparse
 import collections
 import torch.nn as nn
 import numpy as np
+import gc
 from uer.utils.vocab import Vocab
 from uer.utils.constants import *
 from uer.utils.tokenizer import *
@@ -23,7 +24,40 @@ from uer.utils.config import load_hyperparam
 from uer.utils.seed import set_seed
 from uer.model_saver import save_model
 from run_classifier import *
+from data_loader import multi_cls_loader
+from sklearn.model_selection import StratifiedKFold, GroupShuffleSplit
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
 
+def evaluate_sk(args, dataset):
+    src = torch.LongTensor([sample[0] for sample in dataset])
+    tgt = torch.LongTensor([sample[1] for sample in dataset])
+    seg = torch.LongTensor([sample[2] for sample in dataset])
+
+    batch_size = args.batch_size
+    instances_num = src.size()[0]
+
+    y = []
+    y_ = []
+    args.model.eval()
+
+    for i, (src_batch, tgt_batch,  seg_batch, _) in enumerate(batch_loader(batch_size, src, tgt, seg)):
+        src_batch = src_batch.to(args.device)
+        tgt_batch = tgt_batch.to(args.device)
+        seg_batch = seg_batch.to(args.device)
+        with torch.no_grad():
+            loss, logits = args.model(src_batch, tgt_batch, seg_batch)
+        pred = torch.argmax(nn.Softmax(dim=1)(logits), dim=1)
+        gold = tgt_batch
+        for p, g in zip(pred, gold):
+            y.append(g.cpu().numpy())
+            y_.append(p.cpu().numpy())
+    
+    f1 = f1_score(y, y_, average='macro')
+    precision = precision_score(y, y_, average='macro')
+    recall = recall_score(y, y_, average='macro')
+    accuracy = accuracy_score(y, y_)
+
+    return f1, precision, recall, accuracy
 
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -98,33 +132,46 @@ def main():
     parser.add_argument("--folds_num", type=int, default=5,
                         help="The number of folds for cross validation.")
     
+    parser.add_argument('--test_run', type=bool, default=False)
+    
     args = parser.parse_args()
 
+    print('Starting')
     # Load the hyperparameters from the config file.
     args = load_hyperparam(args)
 
     set_seed(args.seed)
-
-    # Count the number of labels. 
-    args.labels_num = count_labels_num(args.train_path)
-
+    # change for short test run
+    if args.test_run:
+        args.epoch = 1
     # Build tokenizer.
     args.tokenizer = globals()[args.tokenizer.capitalize() + "Tokenizer"](args)
 
-    # Training phase.
-    dataset = read_dataset(args, args.train_path)
+    # Training phase. Count the number of labels. 
+    dataset, labels, groups, args.labels_num = multi_cls_loader(args, args.train_path)
+    print('Total sample number: {}'.format(len(dataset)))
     instances_num = len(dataset)
     batch_size = args.batch_size
-    instances_num_per_fold = instances_num // args.folds_num + 1
+
 
     args.train_steps = int(instances_num * args.epochs_num / batch_size) + 1
 
     train_features = []
+    train_features_idxs = []
 
     total_loss, result = 0., 0.
     acc, marco_f1 = 0., 0.
 
-    for fold_id in range(args.folds_num):
+    print('StratifiedKFold for CV.')
+    kf = StratifiedKFold(n_splits=args.folds_num, shuffle=True, random_state=args.seed)
+    dataset = np.array(dataset, dtype=object)
+    labels = np.array(labels)
+    groups = np.array(groups)
+    fold_id = 0
+    for train_index, test_index in kf.split(dataset, labels, groups=groups):
+        X_train, X_val = dataset[train_index], dataset[test_index]
+        y_train, y_val = labels[train_index], labels[test_index]
+        g_train, g_val = groups[train_index], groups[test_index]
         # Build classification model.
         model = Classifier(args)
 
@@ -143,8 +190,7 @@ def main():
             model = torch.nn.DataParallel(model)
         args.model = model
 
-        trainset = dataset[0:fold_id*instances_num_per_fold] + dataset[(fold_id+1)*instances_num_per_fold:]
-        random.shuffle(trainset)
+        trainset = X_train
         
         train_src = torch.LongTensor([example[0] for example in trainset])
         train_tgt = torch.LongTensor([example[1] for example in trainset])
@@ -155,7 +201,7 @@ def main():
         else:
             train_soft_tgt = None
 
-        devset = dataset[fold_id*instances_num_per_fold:(fold_id+1)*instances_num_per_fold]
+        devset = X_val
 
         dev_src = torch.LongTensor([example[0] for example in devset])
         dev_tgt = torch.LongTensor([example[1] for example in devset])
@@ -176,31 +222,32 @@ def main():
              src_batch = src_batch.to(args.device)
              seg_batch = seg_batch.to(args.device)
              with torch.no_grad():
-                 _, logits = model(src_batch, None, seg_batch)
+                _, logits = model(src_batch, None, seg_batch)
              prob = nn.Softmax(dim=1)(logits)
              prob = prob.cpu().numpy().tolist()        
              train_features.extend(prob)
+        for i in g_val:
+            train_features_idxs.append(i)
 
         output_model_name = ".".join(args.output_model_path.split(".")[:-1])
         output_model_suffix = args.output_model_path.split(".")[-1]
         save_model(model, output_model_name+"-fold_"+str(fold_id)+"."+output_model_suffix)
-        result = evaluate(args, devset)
-        acc += result[0]/args.folds_num
-        f1 = []
-        confusion = result[1]
-        for i in range(confusion.size()[0]):
-            p = confusion[i,i].item()/confusion[i,:].sum().item()
-            r = confusion[i,i].item()/confusion[:,i].sum().item()
-            f1.append(2*p*r/(p+r))
-
-        marco_f1 += sum(f1)/len(f1)/args.folds_num
-        # print("Acc. : {:.4f}".format(result[0]))
-        # print("Marco F1 : {:.4f}".format(sum(f1)/len(f1)))
+        f1, precision, recall, accuracy = evaluate_sk(args, devset)
+        print(
+                u'val_f1: %.5f, 'u'val_precision: %.5f, 'u'val_recall: %.5f, 'u'val_accuracy: %.5f\n' %
+                (f1, precision, recall, accuracy,)
+            )
+        args.model = None
+        gc.collect()
+        del model
+        torch.cuda.empty_cache()
+        fold_id += 1
         
     train_features = np.array(train_features)
+    train_features_idxs = np.array(train_features_idxs)
     np.save(args.train_features_path, train_features)
-    print("Acc. : {:.4f}".format(acc))
-    print("Marco F1 : {:.4f}".format(marco_f1))
+    np.save(args.train_features_path+'idx_order', train_features_idxs)
+
 
 
 if __name__ == "__main__":
