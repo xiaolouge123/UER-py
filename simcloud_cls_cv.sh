5c63621e9e07f4393bda4eb1bb3b47533521db05
root <root@9f29bba7-d6cd-4bae-882a-b597311021ea.apple.com>
2020-11-28 10:45:04 +0000

    modify the convert code  from uer to tf with tencent 36 layer bert, add an experiment run script

--- simcloud_cls_cv.sh
+++ simcloud_cls_cv.sh
@@ -4,7 +4,11 @@ CUDA_VISIBLE_DEVICES=0 python3 run_classifier_cv.py --pretrained_model_path ~/pr
                           --train_path ~/ccf-2020/multi_cls/data/cls_10_data.csv \
                           --train_features_path ./stacking_features/tencent_xlarge_5kfold_featrues.txt \
                           --seq_length 512 \
-                          --epochs_num 5 \
+                          --epochs_num 20 \
                           --batch_size 4 \
-                          --test_run True \
                           --encoder bert
+
+
+python convert_bert_from_uer_to_google.py --layers_num 36 \
+                                          --input_model_path /root/pretrain_model/tencent/mixed_corpus_bert_xlarge_model.bin \
+                                          --output_model_path /root/pretrain_model/tencent/tf_version/bert_model.ckpt
